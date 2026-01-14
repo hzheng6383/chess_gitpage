6b17d18654c6815cb00e1f5bfa40ca9f4b02a4a0
Harry <hzheng6383@gmail.com>
2026-01-13 21:05:50 -0500

    Updates

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.chess',
-  appName: 'chess',
-  webDir: 'dist'
-};
-
-export default config;
